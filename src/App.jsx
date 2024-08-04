ab71ddd5bfdb56cf4cdfbc046b73925d95db4438
vinod_mestha <vinodmestha399@gmail.com>
2024-08-04 17:02:29 +0530

    new changes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react"
 import "./index.css"
+import TailButton from "../lib"
 
-export default function TailButton({ label }) {
+export default function App() {
     return (
         <button className='border bg-blue-400 text-white py-2 px-5 rounded-full'>
-            {label ?? "submit"}
+            <TailButton label="testinnnnnnnnnnnnnnnnnnnng" />
         </button>
     )
 }
--- vite.config.js
+++ vite.config.js
@@ -8,10 +8,18 @@ import { libInjectCss } from "vite-plugin-lib-inject-css";
 export default defineConfig( {
   build: {
     lib: {
-      entry: resolve(__dirname, "lib/main.jsx"),
+      entry: resolve(__dirname, "lib/index.jsx"),
       name: 'tail-button',
       fileName: "tail-button"
-    }
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+          globals: {
+              react: "React",
+          },
+      },
+  },
   },
   css: {
     postcss: {
