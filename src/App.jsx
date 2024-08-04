a25ac72886e1f3a999c60cfbf42631a0015b41a3
vinod_mestha <vinodmestha399@gmail.com>
2024-08-04 12:33:25 +0530

    package changes update

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,10 @@
-import TailButton from './TailButton';
+import React from "react"
+import "./index.css"
 
-export default TailButton;
+export default function TailButton({ label }) {
+    return (
+        <button className='border bg-blue-400 text-white py-2 px-5 rounded-full'>
+            {label ?? "submit"}
+        </button>
+    )
+}
--- src/TailButton.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function TailButton({ label }) {
-    return (
-        <button className='border bg-blue-400 text-white py-2 px-5 rounded-full'>
-            {label}
-        </button>
-    )
-}
