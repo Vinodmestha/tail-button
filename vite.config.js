ed085fcfa809bde8926283d430a724c13d6e0e98
vinod_mestha <vinodmestha399@gmail.com>
2024-08-04 13:27:35 +0530

    new build changes updated

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,22 @@
-import {resolve} from 'path'
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
+import react from "@vitejs/plugin-react";
+import { libInjectCss } from "vite-plugin-lib-inject-css";
 
-export default {
+export default defineConfig( {
   build: {
     lib: {
-      entry: resolve(__dirname, "src/app.jsx"),
+      entry: resolve(__dirname, "lib/main.jsx"),
       name: 'tail-button',
-      fileName: "tail"
+      fileName: "tail-button"
     }
-  }
-}
+  },
+  css: {
+    postcss: {
+        plugins: [tailwindcss(), autoprefixer()],
+    },
+  },
+  plugins: [react(), tailwindcss(), libInjectCss()],
+})
